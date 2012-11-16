688116d1438357964a0af0f29a1ec07b7d499823
nozon <nozon@DOUBLEEIGHT01>
2012-11-16 17:58:54 +0100

    Remove useless files.

--- src/profile/__init__.py
+++
@@ -1,7 +0,0 @@
-# Initialize logging system
-import logging
-logger = logging.getLogger("faf.profile")
-
-from playerstats import Statpage
-
-
--- src/replays/__init__.py
+++
@@ -1,6 +0,0 @@
-import logging
-logger = logging.getLogger("faf.replays")
-logger.setLevel(logging.INFO)
-
-from _replayswidget import ReplaysWidget as Replays
-
