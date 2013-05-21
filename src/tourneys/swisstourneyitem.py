17a0db92563d6b065f710c4123413f0a28ba3974
nozon <nozon@DOUBLEEIGHT01>
2013-05-21 11:20:47 +0200

    Changing ranks names.

---
+++ src/tourneys/swisstourneyitem.py
@@ -0,0 +1,160 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2012 Gael Honorez.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0
+# which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/gpl.html
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#-------------------------------------------------------------------------------
+
+
+
+
+
+from PyQt4 import QtCore, QtGui, QtWebKit
+import util
+from tourneys.hosttourneywidget import HostTourneyWidget
+import client
+
+
+class SwissTourneyItemDelegate(QtGui.QStyledItemDelegate):
+    
+    def __init__(self, *args, **kwargs):
+        QtGui.QStyledItemDelegate.__init__(self, *args, **kwargs)
+        self.height = 125
+        
+    def paint(self, painter, option, index, *args, **kwargs):
+        self.initStyleOption(option, index)
+                
+        painter.save()
+        
+        html = QtGui.QTextDocument()
+        html.setHtml(option.text)
+        if self.height < html.size().height() :
+            self.height = html.size().height()
+        
+       
+        option.text = ""  
+        option.widget.style().drawControl(QtGui.QStyle.CE_ItemViewItem, option, painter, option.widget)
+        
+        #Description
+        painter.translate(option.rect.left() + 10, option.rect.top()+10)
+        clip = QtCore.QRectF(0, 0, option.rect.width() - 10 - 5, option.rect.height())
+        html.drawContents(painter, clip)
+  
+        painter.restore()
+        
+
+    def sizeHint(self, option, index, *args, **kwargs):
+        self.initStyleOption(option, index)
+        item = index.model().data(index, QtCore.Qt.UserRole)
+        html = QtGui.QTextDocument()
+        html.setHtml(option.text)
+        return QtCore.QSize(500, int(html.size().height()))
+
+class SwissTourneyItem(QtGui.QListWidgetItem):
+    FORMATTER_SWISS_OPEN = unicode(util.readfile("tournaments/formatters/swiss_open.qthtml"))
+
+    
+    def __init__(self, parent, uid, *args, **kwargs):
+        QtGui.QListWidgetItem.__init__(self, *args, **kwargs)
+
+        self.uid = int(uid)
+        
+        
+        
+        self.parent = parent
+        
+        self.type = None    
+        self.client = None
+        self.title  = None
+        self.description = None
+        self.state  = None
+        self.players = []
+        self.playersname = []
+        
+        self.viewtext = ""
+        self.height = 40
+        self.setHidden(True)
+
+    def update(self, message, client):
+        '''
+        Updates this item from the message dictionary supplied
+        '''
+        self.client  = client
+        old_state       = self.state
+        self.state      = message.get('state', "close")
+        
+        ''' handling the listing of the tournament '''
+        self.title      = message['name']
+        self.type       = message['type']
+        self.url        = message['url']
+        self.description    = message.get('description', "")
+        self.players        = message.get('participants', [])
+
+        if old_state != self.state and self.state == "started" :
+            widget = QtWebKit.QWebView()
+            widget.setUrl(QtCore.QUrl(self.url))
+            self.parent.topTabs.addTab(widget, self.title)
+
+        self.playersname= []
+        for player in self.players :
+            self.playersname.append(player["name"])
+            if old_state != self.state and self.state == "started" and player["name"] == self.client.login :
+                channel = "#" + self.title.replace(" ", "_")
+                self.client.autoJoin.emit([channel])
+                QtGui.QMessageBox.information(self.client, "Tournament started !", "Your tournament has started !\nYou have automatically joined the tournament channel.")
+
+        playerstring = "<br/>".join(self.playersname)
+
+        self.viewtext = self.FORMATTER_SWISS_OPEN.format(title=self.title, description=self.description, numreg=str(len(self.players)), playerstring=playerstring)
+        self.setText(self.viewtext)
+        
+
+    def display(self):
+        return self.viewtext
+ 
+ 
+    def data(self, role):
+        if role == QtCore.Qt.DisplayRole:
+            return self.display()  
+        elif role == QtCore.Qt.UserRole :
+            return self
+        return super(SwissTourneyItem, self).data(role)
+
+
+
+    def permutations(self, items):
+        """Yields all permutations of the items."""
+        if items == []:
+            yield []
+        else:
+            for i in range(len(items)):
+                for j in self.permutations(items[:i] + items[i+1:]):
+                    yield [items[i]] + j
+
+    def __ge__(self, other):
+        ''' Comparison operator used for item list sorting '''        
+        return not self.__lt__(other)
+    
+    
+    def __lt__(self, other):
+        ''' Comparison operator used for item list sorting '''        
+        if not self.client: return True # If not initialized...
+        if not other.client: return False;
+        
+        
+        # Default: Alphabetical
+        return self.title.lower() < other.title.lower()
+    
+
+
